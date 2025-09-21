9f6926c543e2bec3f5b44272d763cccd42e1a517
MiguelB-dev <mbethancourt2005@gmail.com>
2025-09-21 13:19:53 -0400

    Unifica los estilos de academic/professors-summary
    - Cambia el nombre del archivo subjects-professors por professors-
    summary para mejorar la organización.
    
    - Unifica los estilos de academic-summary y professors-summary.

--- assets/js/sign-up.js
+++ assets/js/sign-up.js
@@ -1,4 +1,3 @@
-// Validar el formulario
-
 // TODO: Validar el formulario.
+
 // TODO: Establecer qué pasará cuando esté válido: si enviará al usuario a la pestaña de sign in, si accederá directamente (algo más orgánico), o cualquier otra opción.
